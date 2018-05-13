fd81b13b5330b1b883de34d1fc8bb0c95d973a0a
Andrew Locke <alocke12992@gmail.com>
2018-05-13 14:33:25 -0600

    adds faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+1.times do
+  User.create(
+    email: 'firstPerson@gmail.com',
+    password: 'password'
+  )
+end
+
+1.times do
+  User.create(
+    email: 'secondPerson@gmail.com',
+    password: 'password'
+  )
+end
+
+puts "Two users have been added"
